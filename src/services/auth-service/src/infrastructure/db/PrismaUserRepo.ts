2eb25e021af2daecfe813763a37610200e42e994
AbhilashG12 <abhilashggg15@gmail.com>
2026-03-19 20:31:53 +0530

    Blah Blah Blah

--- src/services/auth-service/src/infrastructure/db/PrismaUserRepo.ts
+++ src/services/auth-service/src/infrastructure/db/PrismaUserRepo.ts
@@ -1,3 +1,56 @@
+// import { PrismaClient } from '@prisma/client'; 
+// import { type IUserRepo } from "../../domain/repos/IUserRepo.js";
+// import { User as UserClass } from "../../domain/entities/User.js";
+
+// export class PrismaUserRepo implements IUserRepo {
+//   private prisma: PrismaClient;
+
+//   constructor() {
+//     this.prisma = new PrismaClient({});
+//   }
+
+//   async findByEmail(email: string): Promise<UserClass | null> {
+//     const data = await this.prisma.user.findUnique({ where: { email } });
+//     if (!data) return null;
+//     return this.mapToDomain(data);
+//   }
+
+//   async findById(id: string): Promise<UserClass | null> {
+//     const data = await this.prisma.user.findUnique({ where: { id } });
+//     if (!data) return null;
+//     return this.mapToDomain(data);
+//   }
+
+//   async save(user: UserClass): Promise<void> {
+//     // Upsert is safer for MongoDB to prevent duplicate ID crashes
+//     await this.prisma.user.upsert({
+//       where: { id: user.props.id },
+//       update: {
+//         email: user.props.email,
+//         passwordHash: user.props.passwordHash,
+//         role: user.props.role,
+//       },
+//       create: {
+//         id: user.props.id,
+//         email: user.props.email,
+//         passwordHash: user.props.passwordHash,
+//         role: user.props.role,
+//         createdAt: user.props.createdAt
+//       }
+//     });
+//   }
+
+//   private mapToDomain(data: any): UserClass {
+//     return new UserClass({
+//       id: data.id,
+//       email: data.email,
+//       passwordHash: data.passwordHash,
+//       role: data.role as "user" | "admin",
+//       createdAt: data.createdAt
+//     });
+//   }
+// }
+
 import { PrismaClient } from '@prisma/client'; 
 import { type IUserRepo } from "../../domain/repos/IUserRepo.js";
 import { User as UserClass } from "../../domain/entities/User.js";
--- src/services/auth-service/src/server.ts
+++ src/services/auth-service/src/server.ts
@@ -1,3 +1,15 @@
+// import {app} from "./app.js";
+// import "dotenv/config.js";
+// import {logger} from "@platform/logger"
+
+
+// const PORT = process.env.PORT || 8001;
+
+// app.listen(PORT, () => {
+//   logger.info(`Auth Service is running on http://localhost:${PORT}`);
+// });
+
+
 import {app} from "./app.js";
 import "dotenv/config.js";
 import {logger} from "@platform/logger"

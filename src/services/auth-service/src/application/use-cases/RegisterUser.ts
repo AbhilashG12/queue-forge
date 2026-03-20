0df8fa722d03a76fd77a73312b3d18f39f9585fa
AbhilashG12 <abhilashggg15@gmail.com>
2026-03-20 20:42:37 +0530

    Blah Blah Blah

--- src/services/auth-service/src/application/use-cases/RegisterUser.ts
+++ src/services/auth-service/src/application/use-cases/RegisterUser.ts
@@ -1,3 +1,42 @@
+// import { type IUserRepo } from '../../domain/repos/IUserRepo.js';
+// import {ConflictError} from "@platform/errors/src/index.js" 
+// import { User as UserClass } from "../../domain/entities/User.js"
+
+// interface RegisterRequest{
+//     email : string;
+//     passwordRaw : string;
+// }
+
+// export class RegisterUser{
+//     constructor(
+//         private userRepo : IUserRepo,
+//         private passwordHasher : any
+//     ){}
+
+//     async execute(request:RegisterRequest){
+//         const exists = await this.userRepo.findByEmail(request.email);
+//         if(exists) throw new ConflictError("User Already Exists !");
+
+//         const hashed = await this.passwordHasher.hash(request.passwordRaw);
+
+//         const user = new UserClass({
+//             id : crypto.randomUUID(),
+//             email : request.email,
+//             passwordHash : hashed,
+//             role : "user",
+//             createdAt : new Date(),
+//         })
+
+//         await this.userRepo.save(user);
+
+//         return {
+//         id: user.props.id,
+//         email: user.props.email,
+//         role: user.props.role
+//         };  
+//     }
+// }
+
 import { type IUserRepo } from '../../domain/repos/IUserRepo.js';
 import {ConflictError} from "@platform/errors/src/index.js" 
 import { User as UserClass } from "../../domain/entities/User.js"
